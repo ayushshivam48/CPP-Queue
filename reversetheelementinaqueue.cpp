3194a99b0141db9bc4630dd8f2797a86f9f9927a
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-01-09 09:29:56 +0530

    Add files via upload

---
+++ reversetheelementinaqueue.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int> input;
+    input.push(10);
+    input.push(20);
+    input.push(30);
+    input.push(40);
+    
+
+    stack<int> st;
+    while (!input.empty()) {
+       st.push(input.front());
+        input.pop();
+    }
+    while (!st.empty()) {
+       input.push(st.top());
+        st.pop();
+    }
+    while (!input.empty()) {
+        cout << input.front() << " ";
+        input.pop();
+    }
+}
